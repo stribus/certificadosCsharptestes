718ca3ea58a0c68ea7d73631af4c34cec2f16002
hagahood <paladinotoldo@gmail.com>
2021-04-24 20:02:03 -0300

    exemplo 1

---
+++ exemplo1X509Store/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+public class Example
+{
+    static void Main()
+    {
+        Console.WriteLine("\r\nExists Certs Name and Location");
+        Console.WriteLine("------ ----- -------------------------");
+
+        foreach (StoreLocation storeLocation in (StoreLocation[])
+            Enum.GetValues(typeof(StoreLocation)))
+        {
+            foreach (StoreName storeName in (StoreName[])
+                Enum.GetValues(typeof(StoreName)))
+            {
+                X509Store store = new X509Store(storeName, storeLocation);
+
+                try
+                {
+                    store.Open(OpenFlags.OpenExistingOnly);
+
+                    Console.WriteLine("Yes    {0,4}  {1}, {2}",
+                        store.Certificates.Count, store.Name, store.Location);
+                }
+                catch (CryptographicException)
+                {
+                    Console.WriteLine("No           {0}, {1}",
+                        store.Name, store.Location);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
